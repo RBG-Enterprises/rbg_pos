acc41fc8cead1cdbec6e5db2d5a8a3e7680a8817
Von Christian Halip <vc.halip@gmail.com>
2022-05-21 20:01:43 +0800

    Change lamut contact number

--- app/pdfs/service_tag_pdf.rb
+++ app/pdfs/service_tag_pdf.rb
@@ -48,7 +48,7 @@ class ServiceTagPdf < Prawn::Document
       move_down 10
       text "LAMUT REPAIR CENTER", size: 10, style: :bold
       move_down 2
-      table([["0917-100-0659"]],cell_style: {font: "Helvetica", :padding => [3,0,0,0]}, column_widths: [150, 100]) do
+      table([["0945-284-8276"]],cell_style: {font: "Helvetica", :padding => [3,0,0,0]}, column_widths: [150, 100]) do
         cells.borders = []
         row(0).font_style = :bold
       end
