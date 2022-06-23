c656316156d7f4ba371dc82069d425782456d36c
Von Christian Halip <vc.halip@gmail.com>
2022-06-23 09:22:43 +0800

    Bump ruby to 3.1.2

--- spec/models/employees/employee_cash_account_spec.rb
+++ spec/models/employees/employee_cash_account_spec.rb
@@ -1,11 +1,14 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 module Employees
   describe EmployeeCashAccount do
-    describe 'associations' do
+    describe "associations" do
       it { is_expected.to belong_to :employee }
       it { is_expected.to belong_to :cash_account }
     end
+
     it '.cash_accounts' do
       cash_account_1 = create(:asset)
       cash_account_2 = create(:asset)
@@ -15,7 +18,7 @@ module Employees
 
       expect(described_class.cash_accounts).to include(cash_account_1)
       expect(described_class.cash_accounts).to include(cash_account_2)
-
+      expect(described_class.cash_accounts).not_to include(cash_account_3)
     end
   end
 end
