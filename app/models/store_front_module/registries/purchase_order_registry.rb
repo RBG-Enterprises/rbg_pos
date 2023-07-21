a794fd6dea33dd66f0bf444b3b4bc748b27c02ef
Von Christian Halip <vc.halip@gmail.com>
2023-07-21 09:25:28 +0800

    Log row

--- app/models/store_front_module/registries/purchase_order_registry.rb
+++ app/models/store_front_module/registries/purchase_order_registry.rb
@@ -9,6 +9,7 @@ module StoreFrontModule
         header = product_spreadsheet.row(1)
         (2..product_spreadsheet.last_row).each do |i|
           row = Hash[[header, product_spreadsheet.row(i)].transpose]
+          Rails.logger.info(row)
           create_or_find_product(row)
           create_or_find_line_item(row)
           find_or_create_selling_price(row)
