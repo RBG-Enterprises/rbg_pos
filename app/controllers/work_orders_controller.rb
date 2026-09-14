34f79c477032d938ecf8b3c72e4c7cf9d4d0734d
Von Christian Halip <vc.halip@gmail.com>
2026-09-14 18:19:07 +0800

    Add csv option for repairs report

--- app/controllers/work_orders_controller.rb
+++ app/controllers/work_orders_controller.rb
@@ -11,6 +11,7 @@ class WorkOrdersController < ApplicationController
         pdf = WorkOrderPdf.new(@work_orders, @from_date, @to_date, view_context)
         send_data pdf.render, type: "application/pdf", disposition: 'inline', file_name: "Work Order.pdf"
       end
+      format.csv { render_csv }
     end
   end
   def new
@@ -30,5 +31,60 @@ class WorkOrdersController < ApplicationController
   private
   def work_order_params
     params.require(:work_order_form).permit(:description, :model_number, :serial_number, :physical_condition, :reported_problem)
+  end
+
+  def render_csv
+    headers.delete("Content-Length")
+    headers["Cache-Control"] = "no-cache"
+    headers["Content-Type"] = "text/csv"
+    headers["Content-Disposition"] = "attachment; filename=\"Work Order.csv\""
+    headers["X-Accel-Buffering"] = "no"
+
+    self.response_body = csv_body
+
+    response.status = 200
+  end
+
+  def csv_body
+    Enumerator.new do |yielder|
+      yielder << CSV.generate_line([
+        "DATE",
+        "CUSTOMER",
+        "#",
+        "UNIT",
+        "STATUS",
+        "REPORTED PROBLEM",
+        "TECHNICIANS",
+        "SPARE PARTS",
+        "CHARGES",
+        "TOTAL COST",
+        "RECEIVED DATE",
+        "RELEASE DATE",
+      ])
+      @work_orders.each do |work_order|
+        yielder << CSV.generate_line([
+          work_order.created_at.strftime("%B %e, %Y"),
+          work_order.customer_name,
+          work_order.service_number,
+          work_order.product_unit.try(:description),
+          work_order.status.try(:titleize),
+          work_order.reported_problem,
+          work_order.technicians_name,
+          work_order.total_spare_parts_cost,
+          work_order.total_service_charges_cost,
+          work_order.total_charges_cost,
+          work_order.created_at.strftime("%B %e, %Y"),
+          work_order.release_date.try(:strftime, "%B %e, %Y"),
+        ])
+      end
+      yielder << CSV.generate_line([
+        "", "", "", "", "", "",
+        @work_orders.total_spare_parts_cost(from_date: @from_date, to_date: @to_date),
+        @work_orders.total_service_charges_cost(from_date: @from_date, to_date: @to_date),
+        @work_orders.total_charges_cost(from_date: @from_date, to_date: @to_date),
+        "",
+        "",
+      ])
+    end
   end
 end
