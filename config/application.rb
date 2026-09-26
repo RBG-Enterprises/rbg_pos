63b193ea029e84cd4f7210a5480d35ca3dd976e8
Von Christian Halip <vc.halip@gmail.com>
2026-09-26 22:19:52 +0800

    Phase 4: solid_queue+solid_cache+solid_cable (single shared PG), recurring inventory rebuild, drop sidekiq/redis/dump.rdb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module RbgPos
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :solid_queue
   end
 end
